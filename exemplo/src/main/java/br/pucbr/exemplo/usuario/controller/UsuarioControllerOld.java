fc51820be789b93b230f47bec9d9eb9c5b483825
Cleverson Ferreira <cleverson.ferreira.br@AC-LPT-00220.local>
2022-08-12 21:53:50 -0300

    fixes

--- exemplo/src/main/java/br/pucbr/exemplo/usuario/controller/UsuarioControllerOld.java
+++ exemplo/src/main/java/br/pucbr/exemplo/usuario/controller/UsuarioControllerOld.java
@@ -1,34 +1,31 @@
 package br.pucbr.exemplo.usuario.controller;
 
 import br.pucbr.exemplo.usuario.entity.Usuario;
+import br.pucbr.exemplo.usuario.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-@RequestMapping("/usuario")
-public class UsuarioController {
+//@RestController
+//@RequestMapping("/usuario")
+public class UsuarioControllerOld {
+
+    @Autowired
+    UsuarioService usuarioService;
 
     @PostMapping
-    public Usuario salvar(@RequestBody Usuario usuario) {
-        System.out.println(usuario);
-
-        return usuario;
+    public void salvar(@RequestBody Usuario usuario) {
+        usuarioService.salvar(usuario);
     }
 
     @GetMapping
     public List<Usuario> listar() {
-        Usuario u = new Usuario();
-        u.setId(1);
-
-        List<Usuario> usuarios = new ArrayList<>();
-        usuarios.add(u);
-        return usuarios;
+        return usuarioService.listar();
     }
 
 }
