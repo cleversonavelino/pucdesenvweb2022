0ad6d96889dabd026ebcbb0c2b4261e90bcff98c
Cleverson Ferreira <cleverson.ferreira.br@AC-LPT-00220.local>
2022-09-16 22:07:32 -0300

    eureka

---
+++ register/src/main/java/br/pucpr/register/RegisterApplication.java
@@ -0,0 +1,15 @@
+package br.pucpr.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterApplication.class, args);
+	}
+
+}
---
+++ register/src/test/java/br/pucpr/register/RegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucpr.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
